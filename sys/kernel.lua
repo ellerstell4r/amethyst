c6b091ea178d61b67ce22cf7cc009c99eb36fa1e
ellerstell4r <notypefyp@proton.me>
2026-01-07 21:16:22 +0600

    Create kernel.lua

---
+++ sys/kernel.lua
@@ -0,0 +1,20 @@
+local kernel = {}
+
+function kernel.run(raw_loadfile, term)
+    amethyst.api.sys = {
+        execute = function(path, ...)
+            local code, err = raw_loadfile(path)
+            if not code then return false, err end
+            local result = { pcall(code, ...) }
+            if not result[1] then
+                return false, result[2]
+            end
+            return true
+        end
+    }
+
+    term.setColor(0x00FF00) term.write("task: "); term.setColor(0xFFFFFF); term.write("init install\n")
+    amethyst.api.sys.execute("/sys/install.lua")
+end
+
+return kernel
